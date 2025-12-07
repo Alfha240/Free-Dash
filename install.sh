0ecd9c76d90cf345e5b0c47e97786d5b1aad7026
Alfha240 <ssurajdewangan2244@gmail.com>
2025-12-07 18:48:46 +0530

    insta

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aa71cb131100008a2411191968ada9376d42d3d72d6a8ebd741ee2c684b4184
-size 7781
+oid sha256:b7ec8db6bb92be38e53be08a62772619ce66432b03cfc6da48e02e4056013c82
+size 8684
